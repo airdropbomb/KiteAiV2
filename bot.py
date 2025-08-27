f4229706636a1742d703e7a3bf20abd3d467d3ba
airdropbomb <saginaw135@gmail.com>
2025-08-27 16:27:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -213,7 +213,7 @@ class KiteAI:
          """
      )
 
-      def format_seconds(self, seconds):
+    def format_seconds(self, seconds):
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
